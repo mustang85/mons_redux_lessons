4f955b5da75181dc21361737efd0907af91cfc1e
mustang85 <sergeant-kir@mail.ru>
2017-03-06 19:07:25 +0300

    Added triger events

--- src/app/App.js
+++ src/app/App.js
@@ -4,12 +4,19 @@ import { connect } from 'react-redux';
 import './style.css';
 
 class App extends Component {
+  addTrack = () => {
+    const { onAddTrack } = this.props;
+    console.log('addTrack', this.inputTrack.value);
+    this.props.onAddTrack(this.inputTrack.value);
+    this.inputTrack.value = '';
+  }
   render() {
     const { testStore } = this.props;
+
     return (
       <div className="container">
-      <input type="text" className="trackInput" />
-      <button className="addTrack">Add track</button>
+      <input type="text" className="trackInput" ref={(input) => this.inputTrack = input} />
+      <button className="addTrack" onClick={this.addTrack}>Add track</button>
         <ul className="list">
           {testStore.map((track, id) =>
             <li key={id}>{track}</li>
@@ -24,5 +31,9 @@ export default connect(
   state => ({
     testStore: state
   }),
-  dispatch => ({})
+  dispatch => ({
+    onAddTrack: (trackName) => {
+      dispatch({ type: 'ADD_TRACK', payload: trackName })
+    }
+  })
 )(App);
